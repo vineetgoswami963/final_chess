1e1a10373309801ea8a662c37b28db4663421878
vineetgoswami963 <130813310+vineetgoswami963@users.noreply.github.com>
2025-09-05 02:56:54 +0530

    Update socket.js

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,27 +1,7 @@
-// import io from 'socket.io-client'
-
-// // const URL = 'http://localhost:8000/' 
-// const URL = 'https://chessbackend-production.up.railway.app' 
-
-// const socket = io(URL)
-
-// var mySocketId
- 
-
-// socket.on("createNewGame", statusUpdate => {
-//     console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
-//     mySocketId = statusUpdate.mySocketId
-// })
-
-// export {
-//     socket,
-//     mySocketId
-// }
-
 import io from 'socket.io-client'
 
-// Ensure this has the full protocol prefix
-const URL = 'chessbackend-production.up.railway.app' 
+// const URL = 'http://localhost:8000/' 
+const URL = 'https://chessbackend-production.up.railway.app' 
 
 const socket = io(URL)
 
@@ -37,3 +17,23 @@ export {
     socket,
     mySocketId
 }
+
+// import io from 'socket.io-client'
+
+// // Ensure this has the full protocol prefix
+// const URL = 'chessbackend-production.up.railway.app' 
+
+// const socket = io(URL)
+
+// var mySocketId
+ 
+
+// socket.on("createNewGame", statusUpdate => {
+//     console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
+//     mySocketId = statusUpdate.mySocketId
+// })
+
+// export {
+//     socket,
+//     mySocketId
+// }
