f7e797f3dd756af28c924cd05ffa3f09c0632f46
vineet goswami <vineet.goswami963@gmail.com>
2025-09-05 02:42:40 +0530

    hiugiug

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,7 +1,27 @@
+// import io from 'socket.io-client'
+
+// // const URL = 'http://localhost:8000/' 
+// const URL = 'https://chessbackend-production.up.railway.app' 
+
+// const socket = io(URL)
+
+// var mySocketId
+ 
+
+// socket.on("createNewGame", statusUpdate => {
+//     console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
+//     mySocketId = statusUpdate.mySocketId
+// })
+
+// export {
+//     socket,
+//     mySocketId
+// }
+
 import io from 'socket.io-client'
 
-// const URL = 'http://localhost:8000/' 
-const URL = 'http://chessbackend-production.up.railway.app' 
+// Ensure this has the full protocol prefix
+const URL = 'https://chessbackend-production.up.railway.app' 
 
 const socket = io(URL)
 
